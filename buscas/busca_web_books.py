e453a73f904d635ed7045b7c3ba2af2d46b750a3
Bruna Dias Pupo <brunapupopupo@gmail.com>
2024-04-09 12:51:23 -0300

    Modificações aplicadas na formatação e contractions adiconadas em tratamento.

--- buscas/busca_web_books.py
+++ buscas/busca_web_books.py
@@ -1,102 +1,143 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import contractions
 import time
 import re  # Importe o módulo re
 
 # O webscraping, também conhecido como raspagem de dados da web, é uma técnica utilizada para extrair informações de páginas da internet.
 # Selenium - literalmente abre um googleChrome para buscar as coisa que queremos
 # Service é usado apra abrir uma instância do Chome WebDriver
-service =  Service()
+service = Service()
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
 
-# Manipular a instância os dados 
+# Manipular a instância os dados
 
-url = 'https://books.toscrape.com/'
+url = "https://books.toscrape.com/"
 driver.get(url)
 html_page = driver.page_source
-#print(html_page)
 
-# 01 - Título de todos os livros da Página. 
+
+# 01 - Título de todos os livros da Página.
 regex_titulo = r'<h3><a .*? title="(.*?)">'
 match_titulo = re.findall(regex_titulo, html_page, re.DOTALL)
-
-print('01 - Título de todos os livros da Página. \n')
+print("01 - Título de todos os livros da Página. \n")
 for titulo in match_titulo:
     print(titulo)
-print('____________________________________________________________________________________\n')
+print(
+    "____________________________________________________________________________________\n"
+)
 
-# Tokenização é basicamente converter um texto em uma lista de palavras ou símbolos. 
-# O processo de tokenização divide o título em palavras individuais.
 # 02 - Tokenização aplicado nos títulos dos livros e caracteres convertidos pra minúsculas.
 regex_token_titulo = r'<h3><a .*? title="(.*?)">'
 match_token_titulo = re.findall(regex_token_titulo, html_page, re.DOTALL)
-
-print('02 - Tokenização aplicado nos títulos dos livros e caracteres convertidos pra minúsculas: \n')
+print(
+    "02 - Tokenização aplicado nos títulos dos livros e caracteres convertidos pra minúsculas: \n"
+)
+print(
+    "A tokenização divide os títulos dos livros em tokens, ou seja, unidades menores, facilitando\na análise textual. Ao converter os títulos para minúsculas, reduzimos a variabilidade\n do texto, melhorando a uniformidade para processos de análise em processamento de linguagem natural.\n"
+)
 for token in match_token_titulo:
     tokenizacao = token.lower()
-    tokenizacao = tokenizacao.replace(' ', '\n')
+    tokenizacao = tokenizacao.replace(" ", "\n")
     print(tokenizacao)
-print('____________________________________________________________________________________\n')
+print(
+    "____________________________________________________________________________________\n"
+)
 
-# 03 - Preço dos livros da Página. 
-regex_preco_livro = r'£(\d+\.\d{2})'
+# 03 - Preço dos livros da Página.
+regex_preco_livro = r"£(\d+\.\d{2})"
 match_preco_livro = re.findall(regex_preco_livro, html_page, re.DOTALL)
-
-print('03 - Preço dos livros da Página: \n')
+print("03 - Preço dos livros da Página: \n")
 for preco_livro in match_preco_livro:
     print(preco_livro)
-print('____________________________________________________________________________________\n')
+print(
+    "____________________________________________________________________________________\n"
+)
 
 # 04 - Limpeza Substituição de ponto por vírgula nos preços dos livros da página:
-regex_preco_livro = r'£(\d+\.\d{2})'
+regex_preco_livro = r"£(\d+\.\d{2})"
 match_preco_livro = re.findall(regex_preco_livro, html_page, re.DOTALL)
-print('04 - Limpeza Substituição de ponto por vírgula nos preços dos livros da página: \n')
+print(
+    "04 - Limpeza Substituição de ponto por vírgula nos preços dos livros da página: \n"
+)
 for preco in match_preco_livro:
-    preco_modificado = preco.replace('.', ',')
+    preco_modificado = preco.replace(".", ",")
     print(preco_modificado)
-print('____________________________________________________________________________________\n')
-      
-# 05 - Livros com preço de até 20 libras.
-regex_preco_20_libras = r'£(0(\.\d{2})?|1\d\.\d{2}|20\.00)'
-match_preco_20_libras = re.findall(regex_preco_20_libras, html_page, re.DOTALL)
+print(
+    "____________________________________________________________________________________\n"
+)
 
-print('05 - Livros com preço de até 20 libras: \n')
+# 05 - Livros com preço de até 20 libras.
+regex_preco_20_libras = r"£(0(\.\d{2})?|1\d\.\d{2}|20\.00)"
+match_preco_20_libras = re.findall(regex_preco_20_libras, html_page, re.DOTALL)
+print("05 - Livros com preço de até 20 libras: \n")
 for preco_20_libras in match_preco_20_libras:
     if isinstance(preco_20_libras, tuple):
-        print(''.join(preco_20_libras))
+        print("".join(preco_20_libras))
     else:
         print(preco_20_libras)
-print('____________________________________________________________________________________\n')
+print(
+    "____________________________________________________________________________________\n"
+)
 
 # 06 - Busca Todos os Títulos do Menu.
-regex_menu = r'<a href="catalogue/category/books/[^/]+/index\.html">\s*([^<\s].*?)\s*</a>'
+regex_menu = (
+    r'<a href="catalogue/category/books/[^/]+/index\.html">\s*([^<\s].*?)\s*</a>'
+)
 match_menu = re.findall(regex_menu, html_page, re.DOTALL)
-
-print('Busca 06 - Todos os Títulos do Menu: \n')
+print("Busca 06 - Todos os Títulos do Menu: \n")
 for menu in match_menu:
-        print(menu)
-print('____________________________________________________________________________________\n')
+    print(menu)
+print(
+    "____________________________________________________________________________________\n"
+)
 
-
-# Tokenização é basicamente converter um texto em uma lista de palavras ou símbolos. 
-# O processo de tokenização divide o título em palavras individuais.
-# 07 - Tokenização aplicado nos títulos do menu.
-regex_menu_token = r'<a href="catalogue/category/books/[^/]+/index\.html">\s*([^<\s].*?)\s*</a>'
+# 07 - Tokenização aplicado nos títulos do menu para caracteres maiusculos.
+regex_menu_token = r'<h3><a .*? title="(.*?)">'
 match_menu_token = re.findall(regex_menu_token, html_page, re.DOTALL)
-
-print('07 - Tokenização aplicado nos títulos do menu: \n')
+print("07 - Tokenização aplicado nos títulos do menu: \n")
+print(
+    "*** Novamente aplico a tokenização nos títulos do menu, desta vez convertendo o texto em\nunidades menores chamadas tokens e alterando para MAIÚSCULAS. Isso contribui para a análise\ndos dados textuais, permitindo a Contagem de Frequência e facilitando a análise. ***\n"
+)
 for menu_token in match_menu_token:
     tokenizacao_menu = menu_token.upper()
-    tokenizacao_menu = tokenizacao_menu.replace(' ', '\n')
+    tokenizacao_menu = tokenizacao_menu.replace(" ", "\n")
     print(tokenizacao_menu)
-print('____________________________________________________________________________________\n')
+print(
+    "____________________________________________________________________________________\n"
+)
 
+# 08 - Contrações são a fusão de duas palavras em uma, facilitando a fala ou escrita.
+# Este processo pode incluir a remoção de letras e espaços, transformando duas palavras independentes em uma única palavra contraída.
+regex_titulo_contractions = r'<h3><a .*? title="(.*?)">'
+match_titulo_contractions = re.findall(regex_titulo_contractions, html_page, re.DOTALL)
+largura_coluna = 95
+print("08 - Títulos de todos os livros da página com contrações expandidas: \n")
+print(
+    "*** Contrações são a fusão de duas palavras em uma, facilitando a fala ou escrita. \nEste processo pode incluir a remoção de letras e espaços, transformando duas\npalavras independentes em uma única palavra contraída ***\n"
+)
+print(
+    f"{'Título Original'.ljust(largura_coluna)}|{'Título com Contrações Expandidas'.ljust(largura_coluna)}"
+)
+print("-" * (largura_coluna * 2 + 1))  # tabela
+for titulo_contractions in match_titulo_contractions:
+    # Aplicando a expansão de contrações no título
+    titulo_expandido = contractions.fix(titulo_contractions)
+    print(
+        f"{titulo_contractions.ljust(largura_coluna)}|{titulo_expandido.ljust(largura_coluna)}"
+    )
 
+print(
+    "\nEssas modificações contribuem para a normalização e a análise de texto na mineração de texto."
+)
+print(
+    "____________________________________________________________________________________\n"
+)
 
 time.sleep(1000)
-
 # Fecha o navegador
 driver.quit()
+# print(html_page)
--- buscas/mock/busca_web_books_mockados.py
+++ buscas/mock/busca_web_books_mockados.py
@@ -1,81 +1,124 @@
-
+import contractions
 import re
-html_mockado = './mock_html.html'
-with open(html_mockado, 'r', encoding='utf-8') as file:
+
+html_mockado = "./mock_html.html"
+with open(html_mockado, "r", encoding="utf-8") as file:
     mock = file.read()
 
-# 01 - Título de todos os livros da Página. 
+# 01 - Título de todos os livros da Página.
 regex_titulo = r'<h3><a .*? title="(.*?)">'
 match_titulo = re.findall(regex_titulo, mock, re.DOTALL)
-
-print('01 - Título de todos os livros da Página. \n')
+print("01 - Título de todos os livros da Página. \n")
 for titulo in match_titulo:
     print(titulo)
-print('____________________________________________________________________________________\n')
+print(
+    "____________________________________________________________________________________\n"
+)
 
-# Tokenização é basicamente converter um texto em uma lista de palavras ou símbolos. 
-# O processo de tokenização divide o título em palavras individuais.
+
 # 02 - Tokenização aplicado nos títulos dos livros e caracteres convertidos pra minúsculas.
 regex_token_titulo = r'<h3><a .*? title="(.*?)">'
 match_token_titulo = re.findall(regex_token_titulo, mock, re.DOTALL)
-
-print('02 - Tokenização aplicado nos títulos dos livros e caracteres convertidos pra minúsculas: \n')
+print(
+    "02 - Tokenização aplicado nos títulos dos livros e caracteres convertidos pra minúsculas: \n"
+)
+print(
+    "A tokenização divide os títulos dos livros em tokens, ou seja, unidades menores, facilitando\na análise textual. Ao converter os títulos para minúsculas, reduzimos a variabilidade\n do texto, melhorando a uniformidade para processos de análise em processamento de linguagem natural.\n"
+)
 for token in match_token_titulo:
     tokenizacao = token.lower()
-    tokenizacao = tokenizacao.replace(' ', '\n')
+    tokenizacao = tokenizacao.replace(" ", "\n")
     print(tokenizacao)
-print('____________________________________________________________________________________\n')
+print(
+    "____________________________________________________________________________________\n"
+)
 
-# 03 - Preço dos livros da Página. 
-regex_preco_livro = r'£(\d+\.\d{2})'
+# 03 - Preço dos livros da Página.
+regex_preco_livro = r"£(\d+\.\d{2})"
 match_preco_livro = re.findall(regex_preco_livro, mock, re.DOTALL)
-
-print('03 - Preço dos livros da Página: \n')
+print("03 - Preço dos livros da Página: \n")
 for preco_livro in match_preco_livro:
     print(preco_livro)
-print('____________________________________________________________________________________\n')
+print(
+    "____________________________________________________________________________________\n"
+)
 
 # 04 - Limpeza Substituição de ponto por vírgula nos preços dos livros da página:
-regex_preco_livro = r'£(\d+\.\d{2})'
+regex_preco_livro = r"£(\d+\.\d{2})"
 match_preco_livro = re.findall(regex_preco_livro, mock, re.DOTALL)
-print('04 - Limpeza Substituição de ponto por vírgula nos preços dos livros da página: \n')
+print(
+    "04 - Limpeza Substituição de ponto por vírgula nos preços dos livros da página: \n"
+)
 for preco in match_preco_livro:
-    preco_modificado = preco.replace('.', ',')
+    preco_modificado = preco.replace(".", ",")
     print(preco_modificado)
-print('____________________________________________________________________________________\n')
-      
-# 05 - Livros com preço de até 20 libras.
-regex_preco_20_libras = r'£(0(\.\d{2})?|1\d\.\d{2}|20\.00)'
-match_preco_20_libras = re.findall(regex_preco_20_libras, mock, re.DOTALL)
+print(
+    "____________________________________________________________________________________\n"
+)
 
-print('05 - Livros com preço de até 20 libras: \n')
+# 05 - Livros com preço de até 20 libras.
+regex_preco_20_libras = r"£(0(\.\d{2})?|1\d\.\d{2}|20\.00)"
+match_preco_20_libras = re.findall(regex_preco_20_libras, mock, re.DOTALL)
+print("05 - Livros com preço de até 20 libras: \n")
 for preco_20_libras in match_preco_20_libras:
     if isinstance(preco_20_libras, tuple):
-        print(''.join(preco_20_libras))
+        print("".join(preco_20_libras))
     else:
         print(preco_20_libras)
-print('____________________________________________________________________________________\n')
+print(
+    "____________________________________________________________________________________\n"
+)
 
 # 06 - Busca Todos os Títulos do Menu.
-regex_menu = r'<a href="catalogue/category/books/[^/]+/index\.html">\s*([^<\s].*?)\s*</a>'
+regex_menu = (
+    r'<a href="catalogue/category/books/[^/]+/index\.html">\s*([^<\s].*?)\s*</a>'
+)
 match_menu = re.findall(regex_menu, mock, re.DOTALL)
-
-print('Busca 06 - Todos os Títulos do Menu: \n')
+print("Busca 06 - Todos os Títulos do Menu: \n")
 for menu in match_menu:
-        print(menu)
-print('____________________________________________________________________________________\n')
+    print(menu)
+print(
+    "____________________________________________________________________________________\n"
+)
 
-
-# Tokenização é basicamente converter um texto em uma lista de palavras ou símbolos. 
-# O processo de tokenização divide o título em palavras individuais.
-# 07 - Tokenização aplicado nos títulos do menu.
+# 07 - Tokenização aplicado nos títulos do menu para caracteres maiusculos.
 regex_menu_token = r'<h3><a .*? title="(.*?)">'
 match_menu_token = re.findall(regex_menu_token, mock, re.DOTALL)
-
-print('02 - Tokenização aplicado nos títulos dos livros e caracteres convertidos pra minúsculas: \n')
+print("07 - Tokenização aplicado nos títulos do menu: \n")
+print(
+    "*** Novamente aplico a tokenização nos títulos do menu, desta vez convertendo o texto em\nunidades menores chamadas tokens e alterando para MAIÚSCULAS. Isso contribui para a análise\ndos dados textuais, permitindo a Contagem de Frequência e facilitando a análise. ***\n"
+)
 for menu_token in match_menu_token:
     tokenizacao_menu = menu_token.upper()
-    tokenizacao_menu = tokenizacao_menu.replace(' ', '\n')
+    tokenizacao_menu = tokenizacao_menu.replace(" ", "\n")
     print(tokenizacao_menu)
-print('____________________________________________________________________________________\n')
+print(
+    "____________________________________________________________________________________\n"
+)
 
+# 08 - Contrações são a fusão de duas palavras em uma, facilitando a fala ou escrita.
+# Este processo pode incluir a remoção de letras e espaços, transformando duas palavras independentes em uma única palavra contraída.
+regex_titulo_contractions = r'<h3><a .*? title="(.*?)">'
+match_titulo_contractions = re.findall(regex_titulo_contractions, mock, re.DOTALL)
+largura_coluna = 95
+print("08 - Títulos de todos os livros da página com contrações expandidas: \n")
+print(
+    "*** Contrações são a fusão de duas palavras em uma, facilitando a fala ou escrita. \nEste processo pode incluir a remoção de letras e espaços, transformando duas\npalavras independentes em uma única palavra contraída ***\n"
+)
+print(
+    f"{'Título Original'.ljust(largura_coluna)}|{'Título com Contrações Expandidas'.ljust(largura_coluna)}"
+)
+print("-" * (largura_coluna * 2 + 1))  # tabela
+for titulo_contractions in match_titulo_contractions:
+    # Aplicando a expansão de contrações no título
+    titulo_expandido = contractions.fix(titulo_contractions)
+    print(
+        f"{titulo_contractions.ljust(largura_coluna)}|{titulo_expandido.ljust(largura_coluna)}"
+    )
+
+print(
+    "\nEssas modificações contribuem para a normalização e a análise de texto na mineração de texto."
+)
+print(
+    "____________________________________________________________________________________\n"
+)
